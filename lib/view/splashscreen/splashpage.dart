cf4cfb04bec24b6a9a019ef892a838d1a92fb3df
pragyan Maharjan <developer09.leleventures@gmail.com>
2024-07-01 11:00:11 +0545

    splash

---
+++ lib/view/splashscreen/splashpage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // or your preferred background color
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Your logo or image asset
+            Image.asset('assets/logo.png', width: 200, height: 200),
+            SizedBox(height: 24),
+            // Optionally, add a loading indicator or text
+            CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
